2fbdf719d04555cc617148ee1b35ae9516ab0119
unknown <danilukman2206@gmail.com>
2023-08-21 16:32:37 +0700

    asd

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:laundry_ui/utils/constants.dart';
+
+import 'pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      builder: (BuildContext context, Widget? child) {
+        return MaterialApp(
+          title: "Laundry App",
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            scaffoldBackgroundColor: Constants.scaffoldBackgroundColor,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            textTheme: GoogleFonts.poppinsTextTheme(),
+          ),
+          initialRoute: "/",
+          onGenerateRoute: _onGenerateRoute,
+        );
+      },
+    );
+  }
+}
+
+Route<dynamic> _onGenerateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case '/':
+      return MaterialPageRoute(builder: (BuildContext context) {
+        return Home();
+      });
+    default:
+      return MaterialPageRoute(builder: (BuildContext context) {
+        return Home();
+      });
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:laundry_ui/utils/constants.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Constants.primaryColor,
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Positioned(
+                    top: 100.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: Container(
+                      height: 150.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/cloth_faded.png"),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Image.asset(
+                        "assets/images/illustration.png",
+                        scale: 1.1,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 24.0,
+                ),
+                decoration: BoxDecoration(
+                  color: Constants.scaffoldBackgroundColor,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    topLeft: Radius.circular(30.0),
+                  )
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
